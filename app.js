a3d7a4b4b032180301b1c0d0237e5103f86712f9
joaowilian01 <joaowilian01@gmail.com>
2021-01-10 00:05:12 -0300

    Added an event listener to search for to-do

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
+const inputSearchTodo = document.querySelector('.form-search input')
 
 formAddTodo.addEventListener('submit', event => {
   event.preventDefault()
@@ -23,4 +24,22 @@ todosContainer.addEventListener('click', event => {
   if (Array.from(clickedElement.classList).includes('delete')) {
     clickedElement.parentElement.remove()
   }
+})
+
+inputSearchTodo.addEventListener('input', event => {
+  const inputValue = event.target.value.trim().toLowerCase()
+
+  Array.from(todosContainer.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove('d-flex')
+      todo.classList.add('hidden')
+    })
+  
+  Array.from(todosContainer.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove('hidden')
+      todo.classList.add('d-flex')
+    })
 })
